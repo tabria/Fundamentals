31fc888ba5b411a3f57ce20c1c8f10ca55f20752
Stanislav Minchev <forexftg@yahoo.com>
2017-05-26 21:47:40 +0300

    Debit Card Number exercise

---
+++ C#Refresh/CSharpIntro/DebitCardNumber/DebitNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace DebitCardNumber
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            int firstPart = int.Parse(Console.ReadLine());
+            int secondPart = int.Parse(Console.ReadLine());
+
+            var thirdPart = int.Parse(Console.ReadLine());
+            var fourthPart = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstPart:D4} {secondPart:D4} {thirdPart:D4} {fourthPart:D4}");
+
+        }
+    }
+}
